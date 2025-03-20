a25cc854f4bead0c38a13513935ae3dc37e4e427
Semen <507769020q@gmail.com>
2025-03-20 23:23:52 +0200

    mainFunctionalStart

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const size = 200
 let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
-
-for (let i = 0; i <= 3; i++) {
-    for (let j = 0; j <= 3; j++) {
-        ctx.strokeRect(0, 0, size * i, size * j) 
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        let rect = ctx.strokeRect(i * size, j * size, size, size)
     }
 }
+let currM = X
+
+$(document).ready(function () {
+    ("rect").onclick(this)
+})
